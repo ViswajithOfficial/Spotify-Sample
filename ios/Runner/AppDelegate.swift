752e81331f9a81fe43440331605982ba9e244d03
ViswajitOfficial <viswajithkaofficial@gmail.com>
2022-11-10 12:02:47 +0530

    app delegate updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,14 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        if let parameters = appRemote.authorizationParameters(from: url) {
+            let access_token = parameters[SPTAppRemoteAccessTokenKey]
+            appRemote.connectionParameters.accessToken = access_token
+            sessionManager.application(app, open: url, options: options)
+        } 
+        return true
+    }
 }
